9f52f92a5df0b6377a7fb88fec279060e5c5292e
LeDDaniel <lentedaniel2@gmail.com>
2025-02-26 11:46:15 +0100

    213

--- KorTDD/Class1.cs
+++ KorTDD/Class1.cs
@@ -12,12 +12,17 @@ namespace KorTDD
             kerulet = 0.0 ,
             terulet = 0.0;
         public KorClass(double r) { this.sugar = r; }
+        public void setSugar(double r) {this.sugar = r;}  
         public double setKerulet(double r) { //return this.kerulet = 2 * r * Math.PI;
-            return 0.0;
+            if (r <= 0.0) {
+                throw new ArgumentException();
+            }
+            return 2*this.sugar*Math.Round(Math.PI, 2);
         }
         public double setTerulet(double r) {// return this.kerulet = 2 * r * Math.PI;
-            return 0;
+            return 2 * this.sugar * Math.Round(Math.PI, 2); 
         }
         public double getsugar() { return sugar; } 
+
     }
 }
--- KorrTDD_Teszt/UnitTest1.cs
+++ KorrTDD_Teszt/UnitTest1.cs
@@ -1,14 +1,72 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-
+using KorTDD;
+using System.Security.Cryptography.X509Certificates;
 namespace KorrTDD_Teszt
 {
     [TestClass]
     public class KorClass_Test
     {
+        KorClass kor = new KorClass(4.00);
         [TestMethod]
-        public double Kerulet_Test
+        public void setKerulet_teszt_True()
         {
+            double sugar=kor.getsugar();
+            double vartEredm = 25.12,
+                    kapottEredm;
+            kapottEredm=kor.setKerulet(sugar);
+
+            Assert.AreEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void setKerulet_teszt_False_Alatt()
+        {
+            double sugar = kor.getsugar();
+            double vartEredm = 25.11,
+                    kapottEredm;
+            kapottEredm = kor.setKerulet(sugar);
+
+            Assert.AreNotEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void setKerulet_teszt_False_Felett()
+        {
+            double sugar = kor.getsugar();
+            double vartEredm = 25.13,
+                    kapottEredm;
+            kapottEredm = kor.setKerulet(sugar);
+
+            Assert.AreNotEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void setTerulet_teszt_True()
+        {
+            double sugar = kor.getsugar();
+            double vartEredm =50.24,
+                    kapottEredm;
+            kapottEredm = kor.setTerulet(sugar);
+
+            Assert.AreNotEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void setTerulet_teszt_False_Alatt()
+        {
+            double sugar = kor.getsugar();
+            double vartEredm = 50.23,
+                    kapottEredm;
+            kapottEredm = kor.setKerulet(sugar);
+
+            Assert.AreNotEqual(kapottEredm, vartEredm);
+        }
+        [TestMethod]
+        public void seterulet_teszt_False_Felett()
+        {
+            double sugar = kor.getsugar();
+            double vartEredm = 50.25,
+                    kapottEredm;
+            kapottEredm = kor.setKerulet(sugar);
+
+            Assert.AreNotEqual(kapottEredm, vartEredm);
         }
     }
 }
